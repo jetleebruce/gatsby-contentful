08f2c1792e53eee944c07c7b6c6197160b357153
Serge Modin <modinsv@yandex.ru>
2020-07-28 21:09:45 +0300

    Added netlify form

--- src/pages/index.js
+++ src/pages/index.js
@@ -274,6 +274,23 @@ const BlogIndex = ({ data, location }) => {
           )
         })}
       </SectionWrapper>
+      <SectionWrapper>
+        <form name="contact" netlify>
+          <p>
+            <label>
+              Name <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Email <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </SectionWrapper>
     </Layout>
   )
 }
